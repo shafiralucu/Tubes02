e5e4d90b98b8b790ab3db12d456f205ca49d9333
Shafira Nurfahardi <shafiranurfahardi@gmail.com>
2019-10-26 14:12:50 +0700

    tambah gambar

---
+++ app/src/main/java/com/example/tubes02_p3b/Alien.java
@@ -0,0 +1,9 @@
+package com.example.tubes02_p3b;
+
+import android.graphics.RectF;
+
+public class Alien {
+
+    RectF square; //kotak diluar alien agar bs detect alien tertembak atau tidak
+    private float panjangAlien, lebarAlien;
+}
